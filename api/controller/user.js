cb9dd9aa1b658f14c50014c66dad660ea0690a29
alyapr <161105849+alyapr@users.noreply.github.com>
2024-10-31 12:31:30 +0700

    update controller, model

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,1 +1,25 @@
 const User = require("../../model/user");
+const brcypt = require("bcrypt");
+
+const signUp=(req,res)=>{
+    brcypt.hash(req.body.password,10)
+    .then((hash)=>{
+        const user = new User({
+            email : req.body.email,
+            password : hash
+        });
+
+        user.save()
+        .then((result)=>{
+            res.status(202).json({
+                message : "user created"
+            });
+        })
+        .catch((err)=>{
+            res.status(501).jsom({
+                 message : "internal server error"
+            })
+}
+        );
+    });
+};
